fd66e417806ce823d2011f3d97ccf5a8fc8dca30
Kentrao Sekimoto <ken.sekimoto@hpe.com>
2021-03-11 18:04:02 +0900

    Updated variables.tf

--- variables.tf
+++ variables.tf
@@ -2,23 +2,7 @@ variable "region" {
   description = "GCP region name"
   default     = "asia-northeast1"
 }
-variable "project" {
-  description = "GCP project name"
-  default     = "red-minutia-275210"
-}
 variable "zone" {
   description = "GCP zone name"
   default     = "asia-northeast1-c"
-}
-variable "vm_name" {
-  description = "VM name"
-  default     = "ks-terraform-test"
-}
-variable "machine_type" {
-  description = "Machine Type"
-  default     = "f1-micro"
-}
-variable "env" {
-  description = "environment name"
-  default     = "Terraform Demo"
 }
