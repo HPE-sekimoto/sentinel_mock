ed114370ebc42f9ad3a03f78edf5b8e964800497
Kentrao Sekimoto <ken.sekimoto@hpe.com>
2021-03-16 19:56:09 +0900

    Added subnet with firewall.

--- main.tf
+++ main.tf
@@ -22,3 +22,21 @@ module "bucket" {
   project_id = var.PROJECT_ID
   location   = "asia-northeast1"
 }
+
+resource "google_compute_network" "custom-test" {
+  name                    = "log-test-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet-with-logging" {
+  name          = "log-test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = var.region
+  network       = google_compute_network.custom-test.id
+
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+  }
+}
