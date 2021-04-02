8fbaa653798b71e6623490b97b1fba38c17cab8f
Kentrao Sekimoto <ken.sekimoto@hpe.com>
2021-04-02 20:05:41 +0900

    Updated for using gae flexible.

--- main.tf
+++ main.tf
@@ -1,42 +1,89 @@
 // GCP provider
 variable "GOOGLE_CREDENTIALS" {}
-variable "PROJECT_ID" {}
+variable "project" {}
 
 provider "google" {
   //credentials = file("./gcp_credential.json")
   credentials = "${var.GOOGLE_CREDENTIALS}"
-  project     = "${var.PROJECT_ID}"
+  project     = var.project
   region      = var.region
 }
 
-resource "google_app_engine_application" "app" {
-  project     = var.PROJECT_ID
-  location_id = var.region
+# resource "google_app_engine_application" "appflex" {
+#   project     = var.project
+#   location_id = var.region
+# }
+
+resource "google_project_service" "service" {
+  project = var.project
+  service = "appengineflex.googleapis.com"
+
+  disable_dependent_services = false
 }
 
-module "bucket" {
-  source  = "terraform-google-modules/cloud-storage/google//modules/simple_bucket"
-  version = "~> 1.3"
-
-  name       = "example-xxx-bucket"
-  project_id = var.PROJECT_ID
-  location   = "asia-northeast1"
+resource "google_project_iam_member" "gae_api" {
+  project = var.project
+  role    = "roles/compute.networkUser"
+  member  = "serviceAccount:${var.project}@appspot.gserviceaccount.com"
 }
 
-resource "google_compute_network" "custom-test" {
-  name                    = "log-test-network"
-  auto_create_subnetworks = false
-}
+resource "google_app_engine_flexible_app_version" "myapp_v1" {
+  version_id = "v1"
+  project    = google_project_iam_member.gae_api.project
+  service    = "appflex"
+  runtime    = "nodejs"
 
-resource "google_compute_subnetwork" "subnet-with-logging" {
-  name          = "log-test-subnetwork"
-  ip_cidr_range = "10.2.0.0/16"
-  region        = var.region
-  network       = google_compute_network.custom-test.id
-
-  log_config {
-    aggregation_interval = "INTERVAL_10_MIN"
-    flow_sampling        = 0.5
-    metadata             = "INCLUDE_ALL_METADATA"
+  entrypoint {
+    shell = "node ./app.js"
   }
+
+  deployment {
+    zip {
+      source_url = "https://storage.googleapis.com/${google_storage_bucket.bucket.name}/${google_storage_bucket_object.object.name}"
+    }
+  }
+
+  liveness_check {
+    path = "/"
+  }
+
+  readiness_check {
+    path = "/"
+  }
+
+  env_variables = {
+    port = "8080"
+  }
+
+  handlers {
+    url_regex        = ".*\\/my-path\\/*"
+    security_level   = "SECURE_ALWAYS"
+    login            = "LOGIN_REQUIRED"
+    auth_fail_action = "AUTH_FAIL_ACTION_REDIRECT"
+
+    static_files {
+      path = "my-other-path"
+      upload_path_regex = ".*\\/my-path\\/*"
+    }
+  }
+
+  automatic_scaling {
+    cool_down_period = "120s"
+    cpu_utilization {
+      target_utilization = 0.5
+    }
+  }
+
+  noop_on_destroy = true
+}
+
+resource "google_storage_bucket" "bucket" {
+  project = var.project
+  name = "${var.project}-appengine-static-content"
+}
+
+resource "google_storage_bucket_object" "object" {
+  name   = "hello-world.zip"
+  bucket = google_storage_bucket.bucket.name
+  source = "./hello-world.zip"
 }
