ab81989bccd78947ae18cbc7a6eb53d4b6ae1135
Kentrao Sekimoto <ken.sekimoto@hpe.com>
2021-04-05 15:41:40 +0900

    Added ubuntu 2004 lts image.

--- main.tf
+++ main.tf
@@ -14,6 +14,11 @@ provider "google" {
 #   location_id = var.region
 # }
 
+data "google_compute_image" "my_ubuntu-pro-2004-lts" {
+  family  = "ubuntu-pro-2004-lts"
+  project = "ubuntu-os-pro-cloud"
+}
+
 data "google_compute_image" "my_debian_9" {
   family  = "debian-9"
   project = "debian-cloud"
@@ -22,6 +27,28 @@ data "google_compute_image" "my_debian_9" {
 data "google_compute_image" "my_windows_2016" {
   family  = "windows-2016"
   project = "windows-cloud"
+}
+
+resource "google_compute_instance" "vm_ubuntu-pro-2004-lts" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
+  description  = "ubuntu 2004 lts"
+  tags         = ["terraform-test"]
+  boot_disk {
+    initialize_params {
+      image = data.google_compute_image.my_ubuntu-pro-2004-lts.self_link
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+      // Ephemeral IP
+    }
+  }
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro", "monitoring"]
+  }
 }
 
 resource "google_compute_instance" "vm_debian_9" {
