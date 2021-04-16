41d958838d81113c7c6300619d32efe3a9371ca8
Kentrao Sekimoto <ken.sekimoto@hpe.com>
2021-04-16 10:32:28 +0900

    Created for R13

--- main.tf
+++ main.tf
@@ -1,144 +1,24 @@
 // GCP provider
 variable "GOOGLE_CREDENTIALS" {}
 variable "project" {}
+variable "org_id" {}
+variable "billing_account" {}
 
-provider "google" {
-  //credentials = file("./gcp_credential.json")
-  credentials = "${var.GOOGLE_CREDENTIALS}"
-  project     = var.project
-  region      = var.region
-}
+module "project-factory" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 10.1"
 
-resource "google_compute_network" "default" {
-  name                    = var.network_name
-  auto_create_subnetworks = false
-}
+  name                 = "ks-sample-1"
+  random_project_id    = true
+  org_id               = var.org_id
+#  usage_bucket_name    = "ks-sample-1-usage-report-bucket"
+#  usage_bucket_prefix  = "ks/sample/1/integration"
+  billing_account      = var.billing_account
+#  svpc_host_project_id = "shared_vpc_host_name"
 
-resource "google_compute_subnetwork" "default" {
-  name                     = var.network_name
-  ip_cidr_range            = "10.127.0.0/20"
-  network                  = google_compute_network.default.self_link
-  region                   = var.region
-  private_ip_google_access = true
-}
-
-resource "google_compute_router" "default" {
-  name    = "lb-https-redirect-router"
-  network = google_compute_network.default.self_link
-  region  = var.region
-}
-
-module "cloud-nat" {
-  source     = "terraform-google-modules/cloud-nat/google"
-  version    = "1.4.0"
-  router     = google_compute_router.default.name
-  project_id = var.project
-  region     = var.region
-  name       = "cloud-nat-lb-https-redirect"
-}
-
-data "template_file" "group-startup-script" {
-  template = file(format("%s/gceme.sh.tpl", path.module))
-
-  vars = {
-    PROXY_PATH = ""
-  }
-}
-
-module "mig_template" {
-  source     = "terraform-google-modules/vm/google//modules/instance_template"
-  version    = "6.2.0"
-  network    = google_compute_network.default.self_link
-  subnetwork = google_compute_subnetwork.default.self_link
-  service_account = {
-    email  = ""
-    scopes = ["cloud-platform"]
-  }
-  name_prefix    = var.network_name
-  startup_script = data.template_file.group-startup-script.rendered
-  tags = [
-    var.network_name,
-    module.cloud-nat.router_name
-  ]
-}
-
-module "mig" {
-  source            = "terraform-google-modules/vm/google//modules/mig"
-  version           = "6.2.0"
-  instance_template = module.mig_template.self_link
-  region            = var.region
-  hostname          = var.network_name
-  target_size       = 2
-  named_ports = [{
-    name = "http",
-    port = 80
-  }]
-  network    = google_compute_network.default.self_link
-  subnetwork = google_compute_subnetwork.default.self_link
-}
-
-module "gce-lb-http" {
-  source            = "GoogleCloudPlatform/lb-http/google"
-  version           = "~> 4.4"
-  name                 = "ci-https-redirect"
-  project              = var.project
-  target_tags          = [var.network_name]
-  firewall_networks    = [google_compute_network.default.name]
-  ssl                  = true
-  ssl_certificates     = [google_compute_ssl_certificate.example.self_link]
-  use_ssl_certificates = true
-  https_redirect       = true
-
-  backends = {
-    default = {
-      description                     = null
-      protocol                        = "HTTP"
-      port                            = 80
-      port_name                       = "http"
-      timeout_sec                     = 10
-      connection_draining_timeout_sec = null
-      enable_cdn                      = false
-      security_policy                 = null
-      session_affinity                = null
-      affinity_cookie_ttl_sec         = null
-      custom_request_headers          = null
-
-      health_check = {
-        check_interval_sec  = null
-        timeout_sec         = null
-        healthy_threshold   = null
-        unhealthy_threshold = null
-        request_path        = "/"
-        port                = 80
-        host                = null
-        logging             = null
-      }
-
-      log_config = {
-        enable      = false
-        sample_rate = null
-      }
-
-      groups = [
-        {
-          group                        = module.mig.instance_group
-          balancing_mode               = null
-          capacity_scaler              = null
-          description                  = null
-          max_connections              = null
-          max_connections_per_instance = null
-          max_connections_per_endpoint = null
-          max_rate                     = null
-          max_rate_per_instance        = null
-          max_rate_per_endpoint        = null
-          max_utilization              = null
-        }
-      ]
-      iap_config = {
-        enable               = false
-        oauth2_client_id     = ""
-        oauth2_client_secret = ""
-      }
-    }
-  }
+#  shared_vpc_subnets = [
+#    "projects/base-project-196723/regions/us-east1/subnetworks/default",
+#    "projects/base-project-196723/regions/us-central1/subnetworks/default",
+#    "projects/base-project-196723/regions/us-central1/subnetworks/subnet-1",
+#  ]
 }
