8970593f09dd2f9e1076f30dc04519b7976295f9
Kentrao Sekimoto <ken.sekimoto@hpe.com>
2021-05-11 10:04:37 +0900

    Created a template for R14

--- main.tf
+++ main.tf
@@ -2,90 +2,22 @@
 variable "GOOGLE_CREDENTIALS" {}
 variable "project" {}
 
-data "google_iam_policy" "admin" {
-  binding {
-    role = "roles/compute.instanceAdmin"
-
-    members = [
-      "serviceAccount:terraform-admin@ksgadget-admin-1.iam.gserviceaccount.com",
-    ]
-  }
-
-  binding {
-    role = "roles/storage.objectViewer"
-
-    members = [
-      "user:ksgadget@ksgadget.site",
-    ]
-  }
-
-  audit_config {
-    service = "cloudkms.googleapis.com"
-    audit_log_configs {
-      log_type = "DATA_READ"
-      exempted_members = ["user:ksgadget@ksgadget.site"]
-    }
-
-    audit_log_configs {
-      log_type = "DATA_WRITE"
-    }
-
-    audit_log_configs {
-      log_type = "ADMIN_READ"
-    }
-  }
+resource "google_compute_network" "custom-test" {
+  name                    = "log-test-network"
+  auto_create_subnetworks = false
 }
 
-# https://github.com/terraform-google-modules/terraform-google-folders
+resource "google_compute_subnetwork" "subnet-with-logging" {
+  name          = "log-test-subnetwork"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = var.region
+  network       = google_compute_network.custom-test.id
 
-#====================================================================
-# first layer folder creation
-#====================================================================
-
-module "folders1" {
-  source  = "terraform-google-modules/folders/google"
-  version = "~> 2.0"
-
-  parent = var.folders_layer1.parent
-
-  names = var.folders_layer1.folders
-
-  #  set_roles = true
-
-  #  per_folder_admins = {
-  #    dev = "group:gcp-developers@domain.com"
-  #    staging = "group:gcp-qa@domain.com"
-  #    production = "group:gcp-ops@domain.com"
-  #  }
-
-  #  all_folder_admins = [
-  #    "group:gcp-security@domain.com",
-  #  ]
-}
-
-resource "google_folder_iam_audit_config" "folders1_folder" {
-  count = length(var.folders_layer1.folders)
-  folder  = module.folders1.ids_list[count.index]
-  service = "allServices"
-  audit_log_config {
-    log_type = "ADMIN_READ"
-  }
-  audit_log_config {
-    log_type = "DATA_READ"
-  }
-}
-
-resource "google_folder_iam_audit_config" "folders1_folder_specific" {
-  count = length(var.folders_layer1.folders)
-  folder  = module.folders1.ids_list[count.index]
-  service = "appengine.googleapis.com"
-  audit_log_config {
-    log_type = "ADMIN_READ"
-  }
-  audit_log_config {
-    log_type = "DATA_READ"
-  }
-  audit_log_config {
-    log_type = "DATA_WRITE"
+  log_config {
+    aggregation_interval = "INTERVAL_10_MIN"
+    flow_sampling        = 0.5
+    metadata             = "INCLUDE_ALL_METADATA"
+    # metadata_fields = [ "value" ]
+    # filter_expr = "value"
   }
 }
--- variables.tf
+++ variables.tf
@@ -2,31 +2,23 @@ variable "region" {
   description = "GCP region name"
   default     = "asia-northeast1"
 }
+variable "project" {
+  description = "GCP project name"
+  default     = "optimal-mender-309507"
+}
 variable "zone" {
   description = "GCP zone name"
   default     = "asia-northeast1-c"
 }
-variable "location" {
-  description = "location"
-  default = "JP"
+variable "vm_name" {
+  description = "VM name"
+  default     = "ks-terraform-test"
+}
+variable "machine_type" {
+  description = "Machine Type"
+  default     = "f1-micro"
 }
 variable "env" {
   description = "environment name"
   default     = "Terraform Demo"
-}
-variable "organization" {
-  description = "organization name"
-  default     = "organizations/48328552181"
-}
-
-variable "folders_layer1" {
-  description = "folders layer1"
-  type = object({
-    parent  = string
-    folders = list(string)
-  })
-  default = {
-    parent  = "organizations/48328552181"
-    folders = ["Dept abc", "Dept xyz", "Shared IT"]
-  }
 }
