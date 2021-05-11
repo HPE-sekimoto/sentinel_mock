e0066a9bdd19c7c978645ff34ecb473e0d45d75e
Kentrao Sekimoto <ken.sekimoto@hpe.com>
2021-05-11 10:11:33 +0900

    Fixed duplicated project variable.

--- variables.tf
+++ variables.tf
@@ -2,10 +2,6 @@ variable "region" {
   description = "GCP region name"
   default     = "asia-northeast1"
 }
-variable "project" {
-  description = "GCP project name"
-  default     = "optimal-mender-309507"
-}
 variable "zone" {
   description = "GCP zone name"
   default     = "asia-northeast1-c"
