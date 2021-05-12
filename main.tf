efa98fc250465909939e056ced0a19a5d1e72707
Kentrao Sekimoto <ken.sekimoto@hpe.com>
2021-05-12 14:57:25 +0900

    Created a template for R23.

--- main.tf
+++ main.tf
@@ -2,9 +2,11 @@
 variable "GOOGLE_CREDENTIALS" {}
 variable "project" {}
 
-resource "google_storage_bucket" "auto-expire" {
-  name          = "ks123-auto-expiring-bucket"
-  location      = "JP"
+resource "google_storage_bucket" "bucket1" {
+  name          = "ks124_auto_expiring_bucket"
+
+  project = var.project
+  location      = var.region
   force_destroy = true
 
   lifecycle_rule {
@@ -15,4 +17,19 @@ resource "google_storage_bucket" "auto-expire" {
       type = "Delete"
     }
   }
+}
+
+resource "google_storage_bucket_acl" "image_store_acl" {
+  bucket = google_storage_bucket.bucket1.name
+
+  role_entity = [
+    "OWNER:user-ksekimoto@ksgadget.site",
+    "READER:user-ksgadget@ksgadget.site",
+  ]
+}
+
+resource "google_storage_bucket_access_control" "public_rule" {
+  bucket = google_storage_bucket.bucket1.name
+  role   = "READER"
+  entity = "allUsers"
 }
