7ff062a9e74a38ab8e4b1d21637d17ba687e8d74
Kentrao Sekimoto <ken.sekimoto@hpe.com>
2021-05-14 08:07:38 +0900

    Create a template for R28

--- main.tf
+++ main.tf
@@ -2,166 +2,58 @@
 variable "GOOGLE_CREDENTIALS" {}
 variable "project" {}
 
-#
-# https://github.com/hashicorp/terraform-provider-google/blob/master/examples/cloud-armor/main.tf
-#
-
-resource "random_id" "instance_id" {
-  byte_length = 4
+provider "null" {
+  version = "~> 2.1"
 }
 
-# Set up a backend to be proxied to:
-# A single instance in a pool running nginx with port 80 open will allow end to end network testing
-resource "google_compute_instance" "cluster1" {
-  name         = "armor-gce-${random_id.instance_id.hex}"
-  machine_type = "f1-micro"
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-      # Ephemeral IP
-    }
-  }
-
-  metadata_startup_script = "sudo apt-get update; sudo apt-get install -yq nginx; sudo service nginx restart"
+provider "random" {
+  version = "~> 2.2"
 }
 
-resource "google_compute_firewall" "cluster1" {
-  name    = "armor-firewall"
-  network = "default"
+module "project-factory" {
+  source  = "terraform-google-modules/project-factory/google"
+  version = "~> 10.1"
 
-  allow {
-    protocol = "tcp"
-    ports    = ["80", "43"]
-  }
+  random_project_id       = true
+  name                    = "simple-sample-project"
+  org_id                  = var.organization_id
+  billing_account         = var.billing_account
+  credentials_path        = file("../../../.ssh/gcp_credential.json")
+  default_service_account = "deprivilege"
+
+  activate_api_identities = [{
+    api = "healthcare.googleapis.com"
+    roles = [
+      "roles/healthcare.serviceAgent",
+      "roles/bigquery.jobUser",
+    ]
+  }]
 }
 
-resource "google_compute_instance_group" "webservers" {
-  name        = "instance-group-all"
-  description = "An instance group for the single GCE instance"
-
-  instances = [
-    google_compute_instance.cluster1.self_link,
-  ]
-
-  named_port {
-    name = "http"
-    port = "80"
+resource "google_project_iam_audit_config" "project_allservices" {
+  project = module.project-factory.project_id
+  service = "allServices"
+  audit_log_config {
+    log_type = "ADMIN_READ"
+  }
+  audit_log_config {
+    log_type = "DATA_READ"
+    exempted_members = [
+      "user:ksekimoto@ksgadget.site",
+    ]
   }
 }
 
-resource "google_compute_target_pool" "example" {
-  name = "armor-pool"
-
-  instances = [
-    google_compute_instance.cluster1.self_link,
-  ]
-
-  health_checks = [
-    google_compute_http_health_check.health.name,
-  ]
-}
-
-resource "google_compute_http_health_check" "health" {
-  name               = "armor-healthcheck"
-  request_path       = "/"
-  check_interval_sec = 1
-  timeout_sec        = 1
-}
-
-resource "google_compute_backend_service" "website" {
-  name        = "armor-backend"
-  description = "Our company website"
-  port_name   = "http"
-  protocol    = "HTTP"
-  timeout_sec = 10
-  enable_cdn  = false
-
-  backend {
-    group = google_compute_instance_group.webservers.self_link
+resource "google_project_iam_audit_config" "project_appengine" {
+  project = module.project-factory.project_id
+  service = "appengine.googleapis.com"
+  audit_log_config {
+    log_type = "ADMIN_READ"
   }
-
-  security_policy = google_compute_security_policy.security-policy-1.self_link
-
-  health_checks = [google_compute_http_health_check.health.self_link]
-}
-
-# Cloud Armor Security policies
-resource "google_compute_security_policy" "security-policy-1" {
-  name        = "armor-security-policy"
-  description = "example security policy"
-
-  # Reject all traffic that hasn't been whitelisted.
-  rule {
-    action   = "deny(403)"
-    priority = "2147483647"
-
-    match {
-      versioned_expr = "SRC_IPS_V1"
-
-      config {
-        src_ip_ranges = ["*"]
-      }
-    }
-
-    description = "Default rule, higher priority overrides it"
+  audit_log_config {
+    log_type = "DATA_READ"
   }
-
-  # Whitelist traffic from certain ip address
-  rule {
-    action   = "allow"
-    priority = "1000"
-
-    match {
-      versioned_expr = "SRC_IPS_V1"
-
-      config {
-        src_ip_ranges = var.ip_white_list
-      }
-    }
-
-    description = "allow traffic from 192.0.2.0/24"
+  audit_log_config {
+    log_type = "DATA_WRITE"
   }
-}
-
-# Front end of the load balancer
-resource "google_compute_global_forwarding_rule" "default" {
-  name       = "armor-rule"
-  target     = google_compute_target_http_proxy.default.self_link
-  port_range = "80"
-}
-
-resource "google_compute_target_http_proxy" "default" {
-  name    = "armor-proxy"
-  url_map = google_compute_url_map.default.self_link
-}
-
-resource "google_compute_url_map" "default" {
-  name            = "armor-url-map"
-  default_service = google_compute_backend_service.website.self_link
-
-  host_rule {
-    hosts        = ["mysite.com"]
-    path_matcher = "allpaths"
-  }
-
-  path_matcher {
-    name            = "allpaths"
-    default_service = google_compute_backend_service.website.self_link
-
-    path_rule {
-      paths   = ["/*"]
-      service = google_compute_backend_service.website.self_link
-    }
-  }
-}
-
-output "ip" {
-  value = google_compute_global_forwarding_rule.default.ip_address
 }
--- variables.tf
+++ variables.tf
@@ -6,23 +6,11 @@ variable "zone" {
   description = "GCP zone name"
   default     = "asia-northeast1-c"
 }
-variable "zone_a" {
-  description = "GCP zone name"
-  default     = "asia-northeast1-a"
+variable "organization_id" {
+  description = "The organization id for the associated services"
+  default     = "48328552181"
 }
-variable "zone_b" {
-  description = "GCP zone name"
-  default     = "asia-northeast1-b"
-}
-variable "zone_c" {
-  description = "GCP zone name"
-  default     = "asia-northeast1-c"
-}
-variable "env" {
-  description = "environment name"
-  default     = "Terraform Demo"
-}
-variable "ip_white_list" {
-  description = "A list of ip addresses that can be white listed through security policies"
-  default     = ["192.0.2.0/24"]
+variable "billing_account" {
+  description = "The ID of the billing account to associate this project with"
+  default = "0148D8-5027F4-BCB19F"
 }
